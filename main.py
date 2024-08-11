4d427fceaa780a928df8459c16a174d18ff0f7dc
ynckz <ynckz@yandex.ru>
2024-08-11 19:45:58 +0300

    there is nothing

---
+++ main.py
@@ -0,0 +1,26 @@
+import logging # imports
+import os
+import asyncio
+from handlers import *
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+load_dotenv()
+scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=os.getenv("token"), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
